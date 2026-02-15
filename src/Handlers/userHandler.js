f983df3761704face99352d6269dd0368007e45c
Rudra Verma <rudraverma781@gmail.com>
2026-02-15 14:11:02 +0530

    initial works

--- src/Handlers/userHandler.js
+++ src/Handlers/userHandler.js
@@ -27,7 +27,7 @@ const connectionRequests = async(req,res)=>{
       const connections = await Connection.find({
          toUserId : loggedInUser._id,
          status : "interested"
-      })
+      }).populate("fromUserId",["name","imageUrl","bio","city"])
       res.json({
          message : "Data fetched !",
          data : connections
@@ -40,4 +40,36 @@ const connectionRequests = async(req,res)=>{
       
    }
 }
-module.exports = {profile, connectionRequests}
+
+
+const ConnectedProfiles = async(req,res)=>{
+   const loggedInUser = req.user
+   try {
+       const friends = await  Connection.find({
+           $or:[
+            {toUserId : loggedInUser._id , status : "accepted"},
+            {fromUserId : loggedInUser._id , status : "accepted"},
+           ]
+       }).populate("fromUserId","name , bio , imageUrl").populate("toUserId","name , bio , imageUrl")
+       
+
+       const data = friends.map((row)=>{
+         if(row.fromUserId._id.toString() == loggedInUser._id.toString())
+            return row.toUserId
+         return row.fromUserId
+       })
+
+       res.json({
+         message : "all connected people here !",
+         data
+       })
+   } catch (error) {
+      res.status(500).json({
+         message : "Error !"+error.message,
+      
+       })
+      
+   }
+
+}
+module.exports = {profile, connectionRequests,ConnectedProfiles}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const {profile, connectionRequests} = require("../Handlers/userHandler")
+const {profile, connectionRequests, ConnectedProfiles} = require("../Handlers/userHandler")
 const auth = require("../middlewares/auth")
 
 const userRouter = express.Router()
 
 userRouter.get("/profile",auth,profile)
 userRouter.get("/requests",auth, connectionRequests)
+userRouter.get("/connections",auth, ConnectedProfiles)
 
 
 module.exports = userRouter
